f59e3cb5935e478e875aa82062fcc189c607d50f
Nicole Orbe <nicoleorbe@outlook.com>
2023-04-15 19:25:10 -0500

    added humidity, wind

--- source/index.js
+++ source/index.js
@@ -1,68 +1,15 @@
 let apiKey = "063f2d8d4205c00d9e83991e6beade04";
 
-//display weather for searched city
-function showFahrenheit(response) {
-  let temp = Math.round(response.data.main.temp);
-  let WeatherDescription = response.data.weather[0].description;
-  let WeatherMain = response.data.weather[0].main;
-
-  let temperatureElement = document.querySelector("#current-temp");
-  temperatureElement.innerHTML = `${temp}`;
-
-  let weather = document.querySelector("#current-weather");
-  weather.innerHTML = `${WeatherDescription}`;
-}
-
-function search(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-new-city");
-
-  if (searchInput.value) {
-    let newCity = document.querySelector("#current-city");
-    newCity.innerHTML = `${searchInput.value}`;
-
-    let apiUrlF = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=imperial&appid=${apiKey}`;
-    axios.get(apiUrlF).then(showFahrenheit);
-  }
-}
-
-let searchCity = document.querySelector("#search-bar");
-searchCity.addEventListener("submit", search);
-
-//display F when F is selected
-let displayTempF = document.querySelector("#degree-F");
-displayTempF.addEventListener("click", search);
-
-//display C when C is selected
-function showCelsius(response) {
-  let myTemp = Math.round(response.data.main.temp);
-
-  let temperatureElement = document.querySelector("#current-temp");
-  temperatureElement.innerHTML = `${myTemp}`;
-}
-
-function tempC(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-new-city");
-
-  if (searchInput.value) {
-    let newCity = document.querySelector("#current-city");
-    newCity.innerHTML = `${searchInput.value}`;
-
-    let apiUrlC = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=metric&appid=${apiKey}`;
-    axios.get(apiUrlC).then(showCelsius);
-  }
-}
-
-let displayTempC = document.querySelector("#degree-C");
-displayTempC.addEventListener("click", tempC);
-
 //display weather for current city
 function showCurrentF(response) {
   let myTemp = Math.round(response.data.main.temp);
   let myCity = response.data.name;
   let myWeatherDescription = response.data.weather[0].description;
   let myWeatherMain = response.data.weather[0].main;
+  let myHumidity = response.data.main.humidity;
+  let myWind = response.data.wind.speed;
+
+  myCurrentFahrenheit = Math.round(response.data.main.temp);
 
   let city = document.querySelector("#current-city");
   city.innerHTML = `${myCity}`;
@@ -70,31 +17,10 @@ function showCurrentF(response) {
   temperature.innerHTML = `${myTemp}`;
   let weather = document.querySelector("#current-weather");
   weather.innerHTML = `${myWeatherMain}`;
-
-  if (`${myWeatherMain}` === "Clear") {
-    let newImage = document.querySelector("#img-current-temp");
-    newImage.src = "images/sunny.png";
-  }
-  if (`${myWeatherMain}` === "Clouds") {
-    let newImage = document.querySelector("#img-current-temp");
-    newImage.src = "images/cloud.png";
-  }
-  if (`${myWeatherMain}` === "Rain") {
-    let newImage = document.querySelector("#img-current-temp");
-    newImage.src = "images/sub-cloud-rain.png";
-  }
-  if (`${myWeatherMain}` === "Snow") {
-    let newImage = document.querySelector("#img-current-temp");
-    newImage.src = "images/snow.png";
-  }
-  if (`${myWeatherMain}` === "Thunderstorm") {
-    let newImage = document.querySelector("#img-current-temp");
-    newImage.src = "images/lightning-storm.png";
-  }
-  if (`${myWeatherMain}` === "Drizzle") {
-    let newImage = document.querySelector("#img-current-temp");
-    newImage.src = "images/lightning-storm.png";
-  }
+  let wind = document.querySelector("#current-wind");
+  wind.innerHTML = `Wind: ${myWind}mph`;
+  let humidity = document.querySelector("#current-humidity");
+  humidity.innerHTML = `Humidity: ${myHumidity}%`;
 }
 
 function showPosition(position) {
@@ -110,8 +36,84 @@ function getPosition() {
   navigator.geolocation.getCurrentPosition(showPosition);
 }
 
+myCurrentFahrenheit = null;
+
+//get current location weather on load
+navigator.geolocation.getCurrentPosition(showPosition);
+
+//switch to current location weather
 let currentButton = document.querySelector("#button-current");
 currentButton.addEventListener("click", getPosition);
+
+//convert to celcius
+function showMyCelsius(event) {
+  event.preventDefault();
+  let myTemp = Math.round((myCurrentFahrenheit - 32) / 1.8);
+
+  let temperatureElement = document.querySelector("#current-temp");
+  temperatureElement.innerHTML = `${myTemp}`;
+}
+
+//let displayMyTempC = document.querySelector("#degree-C");
+//displayMyTempC.addEventListener("click", showMyCelsius);
+
+//let showMyFahrenheit = document.querySelector("#degree-F");
+//showMyFahrenheit.addEventListener("click", getPosition);
+
+//display weather for searched city
+function showTemperature(response) {
+  let temp = Math.round(response.data.main.temp);
+  let WeatherDescription = response.data.weather[0].description;
+  let WeatherMain = response.data.weather[0].main;
+  let temperatureElement = document.querySelector("#current-temp");
+  let weather = document.querySelector("#current-weather");
+  let Humidity = response.data.main.humidity;
+  let Wind = response.data.wind.speed;
+
+  showFahrenheit = Math.round(response.data.main.temp);
+
+  temperatureElement.innerHTML = `${temp}`;
+  weather.innerHTML = `${WeatherDescription}`;
+
+  let wind = document.querySelector("#current-wind");
+  wind.innerHTML = `Wind: ${Wind}mph`;
+  let humidity = document.querySelector("#current-humidity");
+  humidity.innerHTML = `Humidity: ${Humidity}%`;
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-new-city");
+
+  if (searchInput.value) {
+    let newCity = document.querySelector("#current-city");
+    newCity.innerHTML = `${searchInput.value}`;
+
+    let apiUrlF = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=imperial&appid=${apiKey}`;
+    axios.get(apiUrlF).then(showTemperature);
+  }
+}
+
+let showFahrenheit = null;
+
+let searchCity = document.querySelector("#search-bar");
+searchCity.addEventListener("submit", search);
+
+//display F when F is selected
+let displayTempF = document.querySelector("#degree-F");
+displayTempF.addEventListener("click", search);
+
+//display C when C is selected
+function showCelsius(event) {
+  event.preventDefault();
+  let myTemp = Math.round((showFahrenheit - 32) / 1.8);
+
+  let temperatureElement = document.querySelector("#current-temp");
+  temperatureElement.innerHTML = `${myTemp}`;
+}
+
+let displayTempC = document.querySelector("#degree-C");
+displayTempC.addEventListener("click", showCelsius);
 
 //Display current day of week and current time
 let displayDate = document.querySelector("#current-datetime");
